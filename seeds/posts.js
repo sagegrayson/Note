103191485de05386a31ae7e61ebb8e56f84fe4b8
Ami Asokumar <asokumar@amis-mbp.myfiosgateway.com>
2021-11-30 04:18:30 -0500

    fixed css, redid the home.handlebars css, and updated genre-details partial. Also added/changed some seeds

--- seeds/posts.js
+++ seeds/posts.js
@@ -1,5 +1,9 @@
 const { Post } = require("../models");
-
+// genre_id: 1 is Rock
+// genre_id: 2 is Rap
+// genre_id: 3 is Country
+// genre_id: 4 is Folk
+// genre_id: 5 is Pop
 const postData = [
   {
     post_text: "I love rock!",
@@ -7,18 +11,28 @@ const postData = [
     user_id: 1,
   },
   {
-    post_text: "I love Rap",
+    post_text: "Freddie Mercury is the best!",
+    genre_id: 1,
+    user_id: 4,
+  },
+  {
+    post_text: "Mac Miller was so sick",
     genre_id: 2,
     user_id: 2,
   },
   {
-    post_text: "I love Country",
+    post_text: "Lil Nas X never misses!",
     genre_id: 3,
     user_id: 3,
   },
   {
-    post_text: "I love Folk",
+    post_text: "John Prine was such a legend :(",
     genre_id: 4,
+    user_id: 3,
+  },
+  {
+    post_text: "Doja Cat goes off so hard!",
+    genre_id: 1,
     user_id: 4,
   },
 ];
--- server.js
+++ server.js
@@ -38,3 +38,8 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening on " + PORT));
 });
+
+//THINGS TO UNCOMMENT/FIX BEFORE PUSHING TO MAIN:
+
+// Uncomment withAuth helper function
+// DELETE .env
