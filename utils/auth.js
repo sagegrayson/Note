d82e93465352f8ae5653cba08c94a5b57147f880
Ami Asokumar <asokumar@amis-mbp.myfiosgateway.com>
2021-11-30 04:26:50 -0500

    deleted .env and uncommented withAuth helper function

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,9 @@
 const withAuth = (req, res, next) => {
-  // if (!req.session.loggedIn) {
-  //   res.redirect("/login");
-  // } else {
-  next();
-  // }
+  if (!req.session.loggedIn) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
 };
-//Uncomment this line when pushing
+
 module.exports = withAuth;
