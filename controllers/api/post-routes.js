de216040f7de65b453110ff3794ee327e5ca6ea7
James Sgarella <Jimbo8702@gmail.com>
2021-11-30 00:37:44 -0500

    pull

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,7 +3,23 @@ const { Post, Genre, User } = require("../../models");
 const withAuth = require("../../utils/auth.js");
 
 //The '/api/posts' endpoint
+router.post("/create", async (req, res) => {
+  try {
+    const postData = await Post.create({
+      user_id: req.body.user_id,
+      post_text: req.body.post_text,
+    });
 
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(postData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 //api/posts/all - gets all posts as json
 router.get("/all", async (req, res) => {
   try {
